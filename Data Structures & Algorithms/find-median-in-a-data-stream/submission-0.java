ab8dbaaa3f1e802db3b6654235106d96bfe98433
sreeja_thukkapuram <sreejathukkapuram@gmail.com>
2026-07-23 20:15:57 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.java
@@ -0,0 +1,33 @@
+
+
+class MedianFinder {
+
+    PriorityQueue<Integer> small;
+    PriorityQueue<Integer> large;
+
+    public MedianFinder() {
+
+        small = new PriorityQueue<>(Collections.reverseOrder()); // Max Heap
+        large = new PriorityQueue<>();                           // Min Heap
+    }
+
+    public void addNum(int num) {
+
+        small.offer(num);
+
+        large.offer(small.poll());
+
+        if (large.size() > small.size()) {
+            small.offer(large.poll());
+        }
+    }
+
+    public double findMedian() {
+
+        if (small.size() > large.size()) {
+            return small.peek();
+        }
+
+        return (small.peek() + large.peek()) / 2.0;
+    }
+}
